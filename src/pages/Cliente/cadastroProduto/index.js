7120c6ec7928188b2d05925f3abacd0fa331c17d
JoaoVictoor20 <joaovictor_silva07@hotmail.com>
2020-11-09 22:16:27 -0300

    Atualização AWS

--- src/pages/Cliente/cadastroProduto/index.js
+++ src/pages/Cliente/cadastroProduto/index.js
@@ -100,7 +100,7 @@ class CadastrarProduto extends Component {
     };
 
     handleSubmit = event => { // Usar isso na rota /id/produtos para usar a rota pro carrinho com metodo post
-        fetch("${process.env.REACT_APP_API_URL}/sistema/cadastro/produtos", {
+        fetch(`${process.env.REACT_APP_API_URL}/sistema/cadastro/produtos`, {
             method: "post",
             body: JSON.stringify(this.state.produto),
             headers: {
